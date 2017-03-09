bf578eb5209f683703e93275b50158b93e81dc6c
BhaumikPatel13 <patel.bhaumik.13986@gmail.com>
2017-03-08 19:53:45 -0700

    Basic Binary Search
    sample

---
+++ binarySearch.cpp
@@ -0,0 +1,35 @@
+//********************************************************************
+
+[1] Basic Binary Search
+
+	Given a sorted array and target , find the index of first occurance
+	of target in array.
+
+//********************************************************************
+
+int binarySearch( const vector<int>& input, const int& target )
+{
+	int lo = 0;
+	int hi = input.size();
+	int result = -1;
+	
+	while( lo <= hi )
+	{
+		int mid = lo + ( hi - lo )/2;
+		if( input[mid] == target )
+		{
+			result = mid;
+			hi = mid - 1;
+		}
+		else if( input[mid] > target )
+		{
+			hi = mid - 1;
+		}
+		else
+		{
+			lo = mid +1;
+		}
+	}
+	
+	return result;
+}
