f218320b9d268bb895f32b7c978ff9a5aed1eef1
BhaumikPatel13 <patel.bhaumik.13986@gmail.com>
2017-03-12 20:01:04 -0700

    Command line commit by bhaumik

--- BinarySearch/binarySearch.cpp
+++ BinarySearch/binarySearch.cpp
@@ -16,7 +16,7 @@ int binarySearch( const vector<int>& input, const int& target )
 	int lo = 0;
 	int hi = input.size();
 	int result = -1;
-	
+
 	while( lo <= hi )
 	{
 		int mid = lo + ( hi - lo )/2;
@@ -34,6 +34,7 @@ int binarySearch( const vector<int>& input, const int& target )
 			lo = mid +1;
 		}
 	}
-	
+
 	return result;
 }
+// addding comment at the end
